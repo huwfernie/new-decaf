357af40e857c7ab0cd81980b7b54923d83228159
huwfernie <huwfernie@gmail.com>
2024-02-06 13:17:23 +0000

    update card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,7 +5,7 @@ function Card() {
 
     return (
         <div className="card">
-            <h3 className="card-headline">Card</h3>
+            <h3 className="card-headline">Card Title</h3>
             <img alt="a bag of coffee" className="card-image" />
             <p className="card-text">Something Here</p>
             <Link text="Learn More" url="" icon="" />
