db47157e6b201169cc19c878283f973e63f2b001
huwfernie <huwfernie@gmail.com>
2024-02-06 15:41:40 +0000

    add ways to brew information

--- src/sections/WaysToBrew/index.js
+++ src/sections/WaysToBrew/index.js
@@ -7,17 +7,17 @@ function WaysToBrew() {
     <Section name="ways-to-brew">
       <div className="grid">
         <div className="grid-left grid-50">
-          <img src="/images/logo192.png" className="App-logo" alt="bag of coffee beans" />
+          <img src="https://www.bialetti.com/media/catalog/product/cache/e8aa104d064dcf81ed9afb1c9c6893f4/m/o/moka_express.png" className="App-logo" alt="bag of coffee beans" />
         </div>
         <div className="grid-right grid-50">
           <h2>Ways to Brew</h2>
-          <h3>Mocha Pot</h3>
-          <p>An Italian classic</p>
+          <h3>Bialetti Moka Express</h3>
+          <p>This is the original coffee maker, a symbol of Made in Italy quality, which offers the experience of the true Italian ritual of preparing a delicious cup of coffee.</p>
         </div>
       </div>
       <div className="link-wrapper">
-        <Link text="Learn More" icon="external" url="" />
-        <Link text="Buy Now" icon="external" url="" />
+        <Link text="Learn More" icon="external" url="https://en.wikipedia.org/wiki/Moka_pot" />
+        <Link text="Buy Now" icon="external" url="https://www.bialetti.com/it_en/moka-express.html" />
       </div>
     </Section>
   );
