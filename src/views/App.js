f60fe73b4cb9954faa471c012bf56e9ba0975168
huwfernie <huwfernie@gmail.com>
2024-02-07 10:26:56 +0000

    Add sticky nav bar

--- src/views/App.js
+++ src/views/App.js
@@ -1,14 +1,17 @@
-// import logo from './logo.svg';
+
+import NavigationMenu from '../components/NavigationMenu';
 import Hero from '../sections/Hero';
 import BeanOfTheWeek from '../sections/BeanOfTheWeek';
 import WaysToBrew from '../sections/WaysToBrew';
 import HallOfFame from '../sections/HallOfFame';
 import Footer from '../sections/Footer';
+
 import './App.css';
 
 function App() {
   return (
     <>
+      <NavigationMenu />
       <Hero />
       <BeanOfTheWeek />
       <WaysToBrew />
