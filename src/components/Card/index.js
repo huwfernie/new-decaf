a32fbca49ec02d2286a4ac1172fe20f8290119bc
huwfernie <huwfernie@gmail.com>
2024-02-07 10:07:43 +0000

    card background colour will match image pixel 00

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,43 @@
+import { useRef } from 'react';
 import Link from '../Link';
 import './styles.scss';
 
 function Card({ title, imageUrl, cardText, linkUrl }) {
+    const canvasRef = useRef(null);
+    const imageRef = useRef(null);
+    const cardRef = useRef(null);
+
+    /**
+     * On Load of image, copy image to canvas (hidden el using css) and read RGB colour value for top left pixel
+     * Set background Colour of the card to match the t/l pixel
+     * Default fallback to "white" if opacity < 1 (calculated using pixel[3] < 255);
+     */
+    function handleLoad() {
+        const testCanvas = canvasRef.current !== null;
+        const testCard = cardRef.current !== null;
+        // imageRef cannot be null at this point as handleLoad can only be called after <img... has loaded
+
+        if (testCanvas && testCard) {
+            const ctx = canvasRef.current.getContext("2d");
+            ctx.drawImage(imageRef.current, 0, 0);
+            let pixel = ctx.getImageData(0, 0, 1, 1).data;
+
+            if (pixel[3] !== 255) {
+                pixel[0] = 238;
+                pixel[1] = 238;
+                pixel[2] = 238;
+            }
+
+            cardRef.current.style.backgroundColor = `rgb(${pixel[0]} ${pixel[1]} ${pixel[2]})`;
+        }
+    }
+
     return (
-        <div className="card">
-            <h3 className="card-headline">{title || "Card Title"}</h3>
-            <img alt="a bag of coffee" className="card-image" src={imageUrl} />
-            <p className="card-text">{cardText || "Something Here"}</p>
+        <div ref={cardRef} className="card">
+            <h3 className="card-headline">{title}</h3>
+            <img ref={imageRef} alt="a bag of coffee" className="card-image" src={imageUrl} onLoad={handleLoad} />
+            <canvas ref={canvasRef} className="card-image-canvas"></canvas>
+            <p className="card-text">{cardText}</p>
             <Link text="Link" url={linkUrl} icon="external" />
         </div>
     );
--- src/sections/HallOfFame/index.js
+++ src/sections/HallOfFame/index.js
@@ -9,25 +9,25 @@ function HallOfFame() {
       <div className="grid">
         <Card
           title="Bialetti Moka Deka Decaffeinated Ground Coffee"
-          imageUrl="https://johnlewis.scene7.com/is/image/JohnLewis/238742028?$rsp-pdp-port-640-82$"
+          imageUrl="/images/external/bialetti_decaf.jpeg"
           cardText="Bialetti's Moka Deka decaffeinated ground coffee has a pleasant aroma and a strong structure, giving you the flavour and pleasure of a real espresso. The medium roasting gives this coffee delicious aromatic notes of cereals and toasted bread. It's perfectly suited to Bialetti's Moka coffee maker and is traditionally roasted in Italy."
           linkUrl="https://www.johnlewis.com/bialetti-moka-deka-decaffeinated-ground-coffee-250g/p5112688"
         />
         <Card
           title="Six 'til Midnight Swiss Water Decaf (Organic)"
-          imageUrl="https://katacoffeeroasters.co.uk/cdn/shop/products/decaf_1024x1024@2x.jpg?v=1633121485"
+          imageUrl="/images/external/six_till_midnight_decaf.webp"
           cardText="It's not often that you get a decaf that can fool the coffee geek in your life - but this one might just do it. It's rich, full bodied and has some distinct notes of dark fruits. It produces a rich crema as an espresso and is a real all rounder."
           linkUrl="https://katacoffeeroasters.co.uk/products/copy-of-peru-david-guevara-armijos"
         />
         <Card
           title="Decaf Coffee"
-          imageUrl="https://volcanocoffeeworks.com/cdn/shop/files/200G_decaf_768x768_crop_center.png?v=1690967577"
+          imageUrl="/images/external/volcano_decaf.webp"
           cardText="This decaf coffee is packed with flavour and smoothness, with notes of maple syrup, green grape and dark chocolate caramel. This coffee is made using the chemical-free C02 water method with beans from Peru. We love this delicious coffee for the flavour without the caffeine."
           linkUrl="https://volcanocoffeeworks.com/products/decaf-coffee"
         />
         <Card
           title="GRIND"
-          imageUrl="https://grind.co.uk/cdn/shop/files/227g_Bag_wholebean_house_1000x.png?v=1675169114"
+          imageUrl="/images/external/grind_decaf.webp"
           cardText="This decaf is the best we've ever had. Processed using the Natural EA method, meaning its good for the environment whilst preserving the coffee's original flavours."
           linkUrl="https://grind.co.uk/products/taster-pack-coffee"
         />
