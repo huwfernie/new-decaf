fa647b0ab8c6e5ff7add109f525a177ee07f9928
huwfernie <huwfernie@gmail.com>
2024-02-09 13:06:05 +0000

    fix typo in manifesto section

--- src/sections/Manifesto/index.js
+++ src/sections/Manifesto/index.js
@@ -5,7 +5,7 @@ function Manifesto() {
   return (
     <Section name="manifesto">
         <p>We stand unapologetically for the calm, the composed, and the caffeine&#8209;conscious.</p>
-        <p>Pause, Discover, and brew better.</p>
+        <p>Pause, discover, and brew better.</p>
     </Section>
   );
 }
