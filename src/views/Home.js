cdfbf8339efca0b9658336c160a0a6f18892e4d3
huwfernie <huwfernie@gmail.com>
2024-02-12 20:36:59 +0000

    rename static data

--- src/views/Home.js
+++ src/views/Home.js
@@ -9,7 +9,7 @@ import Menu from '../sections/shared/Menu';
 import { useHashScroll } from "../hooks/useHashScroll";
 import { useMenu } from "../hooks/useMenu";
 
-const appData = {
+const pageData = {
   beanOfTheWeek: {
     headline: "Decaf Coffee Beans from Lavazza",
     body: "A well-balanced, aromatic blend. Experience an unforgettable, full-bodied flavour. The ideal blend if you want to avoid caffeine without giving up the luxury of Italian coffee.",
@@ -84,17 +84,17 @@ function App() {
       <NavigationBar menu={menu} />
       <Hero />
       <Manifesto />
-      <BeanOfTheWeek data={appData.beanOfTheWeek} />
-      <WaysToBrew data={appData.waysToBrew} />
-      <HallOfFame data={appData.hallOfFame} />
+      <BeanOfTheWeek data={pageData.beanOfTheWeek} />
+      <WaysToBrew data={pageData.waysToBrew} />
+      <HallOfFame data={pageData.hallOfFame} />
       <Footer>
         <li className="legal-item"><strong>Hero Image:</strong>https://www.illy.com/en-ww/coffee/decaffeinated-coffee</li>
-        <li className="legal-item"><strong>Bean of the Week Image:</strong>{appData.beanOfTheWeek.linkUrl}</li>
-        <li className="legal-item"><strong>Ways to Brew Image:</strong>{appData.waysToBrew.buyUrl}</li>
-        <li className="legal-item"><strong>Hall of fame 1:</strong>{appData.hallOfFame[0].url}</li>
-        <li className="legal-item"><strong>Hall of fame 2:</strong>{appData.hallOfFame[1].url}</li>
-        <li className="legal-item"><strong>Hall of fame 3:</strong>{appData.hallOfFame[2].url}</li>
-        <li className="legal-item"><strong>Hall of fame 4:</strong>{appData.hallOfFame[3].url}</li>
+        <li className="legal-item"><strong>Bean of the Week Image:</strong>{pageData.beanOfTheWeek.linkUrl}</li>
+        <li className="legal-item"><strong>Ways to Brew Image:</strong>{pageData.waysToBrew.buyUrl}</li>
+        <li className="legal-item"><strong>Hall of fame 1:</strong>{pageData.hallOfFame[0].url}</li>
+        <li className="legal-item"><strong>Hall of fame 2:</strong>{pageData.hallOfFame[1].url}</li>
+        <li className="legal-item"><strong>Hall of fame 3:</strong>{pageData.hallOfFame[2].url}</li>
+        <li className="legal-item"><strong>Hall of fame 4:</strong>{pageData.hallOfFame[3].url}</li>
         <li className="legal-item"><strong>Favicon:</strong><a href="https://www.flaticon.com/free-icons/coffee" title="coffee icons">Coffee icons created by Freepik - Flaticon</a></li>
       </Footer>
       <Menu menu={menu} />
