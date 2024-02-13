f97f79cb070acbf3823ab4f06017c6946c44f379
huwfernie <huwfernie@gmail.com>
2024-02-13 10:19:26 +0000

    improve useHashScroll lifecycle and behaviour

--- src/hooks/useHashScroll.js
+++ src/hooks/useHashScroll.js
@@ -1,16 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function useHashScroll() {
+    const [hash, setHash] = useState(window.location.hash);
+    
     useEffect(() => {
         try {
+            if (hash !== window.location.hash) {
+                setHash(window.location.hash);
+            }
             if (window.location.hash !== "") {
                 const el = document.querySelector(window.location.hash);
-                el.scrollIntoView();
+                el.scrollIntoView({
+                    behavior: "instant"
+                });
             }
         } catch (error) {
             console.log('Error in useHashScroll');
         }
-    });
+
+        return () => {
+            document.documentElement.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: "instant"
+            });
+        }
+    }, [hash]);
+
 }
 
 export { useHashScroll }
